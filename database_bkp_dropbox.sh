41aaf4c0f7926e87adb79a2df7c8ef96ffa4ac73
Helmar Luiz <helmarluizjr@gmail.com>
2016-08-15 01:46:13 -0400

    removing verboses

--- database_bkp_dropbox.sh
+++ database_bkp_dropbox.sh
@@ -4,8 +4,6 @@
 #
 # Author Helmar Junior <helmarluizjr@gmail.com>
 #
-# Before use this scripts, learn the explanations:
-#
 
 #Default values
 DATE_NOW=$(date +"%Y-%m-%d:%H:%M")
@@ -29,8 +27,6 @@ pg_dump -c --inserts -U postgres sistema >  $LOCAL_BKP_DIR/database_full.sql
 # echo "compressing database"
 tar -czvf $LOCAL_BKP_DIR/$DATE_NOW.tar.gz $LOCAL_BKP_DIR/database_full.sql
 
-# nano ~/.dropbox_uploader
-# OAUTH_ACCESS_TOKEN=QkhK9yWBIoAAAAAAAAADxL7vO6rhqoZoy29-aEKHx7_k7ou2kS7MdYl00Jl8aRgc
 # echo "sending backup!"
 ./dropbox_uploader.sh upload $LOCAL_BKP_DIR/$DATE_NOW.tar.gz $DROPBOX_BKP_DIR
 # echo "backup sended!"
